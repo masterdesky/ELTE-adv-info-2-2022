c078ce3e7f90ca10133fb89d9d8362f2b57e4a1d
masterdesky <masterdesky@gmail.com>
2022-06-22 10:21:38 +0200

    bad burgers

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -6,87 +6,82 @@
 #include "pdeint.hpp"
 
 
-double f_init(const double &x)
-{
-    return 0.2*exp(x);
+// Define Pi with great precision and calculate it during compilation time
+constexpr double pi() { return std::atan(1)*4; }
+
+
+// Set initial conditions for the 1D Burgers' equation
+double f_init(const double &x, const double &nu)
+{   
+    double t = 0;
+    double phi = 
+        exp(- pow((-4*t + x - 2*pi()), 2) / (4 * nu * (t + 1)))
+      + exp(- pow((-4*t + x), 2) / (4 * nu * (t + 1)));
+    double phi_prime =
+      - (-8*t + 2*x) / (4 * nu * (t + 1))
+      * exp(- pow((-4*t + x), 2) / (4 * nu * (t + 1)))
+      - (-8*t + 2*x - 4*pi()) / (4 * nu * (t+1))
+      * exp(- pow((-4 * t + x - 2*pi()), 2) / (4 * nu * (t + 1)));
+    
+    return -2 * nu * phi_prime/phi + 4;
 }
 
 
 // Use the central difference to approximate the derivatives
 auto derivate(const std::vector<double> &u, const Field &x)
 {
+    // Using periodic boundary conditions
     std::vector<double> du (x.N);
-    du[0] = pdeint::fw(u[1], u[0], x.v[1], x.v[0]);
-    for(int i=1; i < x.N-1; i++)
+    for(int i=0; i < x.N; i++)
     {
-        du[i] = pdeint::ce(u[i+1], u[i-1], x.v[i+1], x.v[i-1]);
+        int i1  = ((x.N-1)+i+1)%(x.N-1);
+        int i_1 = ((x.N-1)+i-1)%x.N;
+        du[i] = pdeint::ce(u[i1], u[i_1], x.v[i1], x.v[i_1]);
+
     }
-    du[x.N-1] = pdeint::bw(u[x.N-1], u[x.N-2], x.v[x.N-1], x.v[x.N-2]);
-    
     return du;
 }
 
 
 int main(int argc, char const *argv[])
 {
-    int N = 50;
-    double nu = 0.1;
+    int Nx = 200;
+    int Nt = 50;
+    double nu = 0.8;
 
-    std::ofstream
-        file_ftcs, file_btcs, file_crnc, file_mccr;
-    file_ftcs.open("../data/burgers1d_ftcs.dat");
-    file_btcs.open("../data/burgers1d_btcs.dat");
-    file_crnc.open("../data/burgers1d_crnc.dat");
-    file_mccr.open("../data/burgers1d_mccr.dat");
+    std::ofstream file;
+    file.open("../data/burgers1d.dat");
 
-    auto t = Field(N, 0, 1);
-    auto x = Field(N, 0, 5);
+    auto t = Field(Nt);
+    auto x = Field(Nx, 0, 2*pi());
 
-    std::vector<double> u(N);
-    for(int i=0; i < N; i++) { u[i] = f_init(x.v[i]); }
+    std::vector<double> u(Nx);
+    for(int i=0; i < Nx; i++) { u[i] = f_init(x.v[i], nu); }
+    // Write initial conditions to file
+    for(const auto &ui : u) { file << ui << " "; }
+    file << std::endl;
 
     // Burgers 1D
     // du/dt = nu * du2 / dx2 - u * du/dx
-    std::vector<double> dudt (N);
+    std::vector<double> dudt (Nx);
     std::vector<double> dudx;
     std::vector<double> dudx2;
 
     // Implement the FTCS method
     auto u_curr = u;
-    for(const auto &ti : t.v)
+    for(int ti=1; ti < Nt; ti++)
     {
+        t = t.v[ti];
         dudx = derivate(u_curr, x);
         dudx2 = derivate(dudx, x);
-        for(int i=0; i < N; i++) { dudt[i] = nu * dudx2[i] - u_curr[i] * dudx[i]; }
+        for(int i=0; i < Nx; i++) { dudt[i] = nu * dudx2[i] - u_curr[i] * dudx[i]; }
         
         auto u_old = u_curr;
-        for(int i=0; i < N; i++) { u_curr[i] = u_old[i] + dudt[i] * t.h; }
-        for(const auto &ui : u_curr) { file_ftcs << ui << " "; }
-        file_ftcs << std::endl;
-    }
+        for(int i=0; i < Nx; i++) { u_curr[i] = u_old[i] + dudt[i] * t.h; }
 
-    // Implement the BTCS method
-    auto u_curr = u;
-    for(const auto &ti : t.v)
-    {
-        
-        
-        for(const auto &ui : u_curr) { file_ftcs << ui << " "; }
-        file_btcs << std::endl;
-    }
-
-    // Implement the Crank-Nicolson method
-    auto u_curr = u;
-    for(const auto &ti : t.v)
-    {
-        dudx = derivate(u_curr, x);
-        dudx2 = derivate(dudx, x);
-        for(int i=0; i < N; i++) { dudt[i] = nu * dudx2[i] - u_curr[i] * dudx[i]; }
-        
-        auto u_old = u_curr;
-        for(int i=0; i < N; i++) { u_curr[i] = u_old[i] + dudt[i] * t.h; }
-        for(const auto &ui : u_curr) { file_ftcs << ui << " "; }
-        file_crnc << std::endl;
+        // Write to file
+        for(const auto &ui : u_curr) { file << ui << " "; }
+        file << std::endl;
     }
 
     file.close();
